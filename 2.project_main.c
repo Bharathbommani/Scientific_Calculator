376b72d6e35995f5087c05b36cc5a0e3477f5ec7
Bharathbommani <71547527+Bharathbommani@users.noreply.github.com>
2023-01-11 10:44:33 +0530

    Add files via upload

---
+++ 2.project_main.c
@@ -0,0 +1,17 @@
+//header files
+#include<stdio.h>
+#include"header.h"
+#include<stdlib.h>
+
+
+//main function
+
+int main()
+{
+    while(1)
+    {
+       //main_menu();
+       main_menu();
+    }    
+    
+}
